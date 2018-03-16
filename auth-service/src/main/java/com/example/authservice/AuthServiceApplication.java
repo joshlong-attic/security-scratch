fbda14776b53a9e6f33c7fd0759196adb51767a8
Josh Long <starbuxman@gmail.com>
2018-03-15 20:25:14 -0700

    Auth server is working!

--- auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -1,13 +1,11 @@
 package com.example.authservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,13 +18,14 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -56,20 +55,18 @@ class MainController {
 @RestController
 class ProfileRestController {
 
-	@GetMapping("/resources/userfinfo")
+	@GetMapping("/resources/userinfo")
 	Map<String, String> profile(Principal principal) {
 		return Collections.singletonMap("name", principal.getName());
 	}
 }
 
-
-
 @Configuration
 @EnableResourceServer
-class ResourceServerConfig extends ResourceServerConfiguration {
+class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		http
 				.antMatcher("/resources/**")
 				.authorizeRequests()
@@ -123,7 +120,7 @@ class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 	}
 }
 
-//@Service
+@Service
 class SimpleUserDetailsService implements UserDetailsService {
 
 	private final Map<String, UserDetails> users = new ConcurrentHashMap<>();
@@ -140,23 +137,9 @@ class SimpleUserDetailsService implements UserDetailsService {
 	}
 }
 
-
 @Configuration
 @EnableWebSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	// @formatter:off
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-				.inMemoryAuthentication()
-					.withUser("user").password("password").roles("USER")
-					.and()
-					.withUser("josh").password("password").roles("USER")
-					.and()
-					.withUser("admin").password("password").roles("USER", "ADMIN");
-	}
-	// @formatter:on
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -168,5 +151,4 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.formLogin();
 		//@formatter:on
 	}
-
 }
